2e9e51d4cb3669d718d3cd045836c3aa455d6f64
shola Daropale <sholadennis@yahoo.com>
2022-05-01 13:39:40 +0200

    created strings for the different criteria

---
+++ password/02-Homework/Develop/script.js
@@ -0,0 +1,48 @@
+// user variables:
+
+var confirmLowercase;
+var confirmUppercase;
+var confirmNumber;
+var confirmSpecial
+
+// Assignment Code
+
+number = "0123456789";
+special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+lowercase = "abcdefghijklmnopqrstuvwxyz";
+
+
+
+
+
+// var generateBtn = document.querySelector("#generate");
+
+// function generatePassword() {
+//   console.log("hey");
+
+//   return "generated password";
+// }
+
+
+// // 01 prompt the user for the password criteria
+// // 02 password lenght 8<128
+// // 03 validate the input
+// // 04 generate password based on criteria
+// // 05 displaypassword based on criteria.
+
+
+
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
